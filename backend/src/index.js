baf8e0f345e2788f8a3979078fe4f6f91b2c847a
Suraj Gautam <jayshriram9356@gmail.com>
2026-09-02 23:46:22 +0530

    add health check route

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,6 +32,12 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "Chatify Backend is running 🚀"
+    });
+});
+
 // Production Static Assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
